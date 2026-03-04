b147d38b8b1430237f52ee3b2b2c14000a39e11e
yanazlatanova <yana.zlatanova25@gmail.com>
2026-03-04 19:58:59 +0100

    Add a simple ring carousel

--- src/components/CardRing.tsx
+++ src/components/CardRing.tsx
@@ -1,10 +1,35 @@
 import React from "react"
+import type { User } from '../types/user'
+import { Html } from "@react-three/drei"
 
-export const CardRing: React.FC = () => {
+interface Props {
+    users: User[]
+}
+
+export const CardRing: React.FC<Props> = ({ users }) => {
+    if (users.length === 0) return null
+    const userCount = users.length
+
+    const ring_length = userCount * 4
+    // ring_length = R * 2 * PI
+    const R = ring_length / (Math.PI * 2) // Adjust the radius based on the number of users
+
     return (
-        <mesh position={[2, 0, 0]}>
-            <sphereGeometry args={[1, 32, 32]} />
-            <meshStandardMaterial color="purple" />
-        </mesh>
+        <group>
+            {users.map((user, i) => {
+                const radians = (i / userCount) * (Math.PI * 2)  // angle in radians not degrees       
+                const x = R * Math.cos(radians)
+                const z = R * Math.sin(radians)
+                const y = 0 // rotate on the XZ plane only
+
+                return (
+                    <mesh key={user.id} position={[x, y, z]}>
+                        <sphereGeometry args={[1, 32, 32]} />
+                        <meshStandardMaterial color="yellow" />
+                        <Html>{user.name}</Html>
+                    </mesh>
+                );
+            })}
+        </group>
     )
 }
--- src/pages/MainScene.tsx
+++ src/pages/MainScene.tsx
@@ -2,19 +2,17 @@ import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { CardRing } from '../components/CardRing'
+import { useUsers } from '../context/users'
 
 export const MainScene: React.FC = () => {
+    const { data } = useUsers()
     return (
         <Canvas style={{ width: '100vw', height: '100vh' }}>
             <OrbitControls />
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-            <mesh position={[-2, 0, 0]}>
-                <boxGeometry args={[1, 1, 1]} />
-                <meshStandardMaterial color="red" />
-            </mesh>
 
-            <CardRing />
+            <CardRing users={data ?? []} />
         </Canvas>
     )
 }
