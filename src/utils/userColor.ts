2ef20540fdb4a9d440274dbf80bf40d30e10b4e5
yanazlatanova <yana.zlatanova25@gmail.com>
2026-03-05 10:49:57 +0100

    Fix user color algorithm loop range

--- src/utils/userColor.ts
+++ src/utils/userColor.ts
@@ -1,8 +1,8 @@
 export function getUserColor(username: string): string {
   if (!username) return '#cccccc'
-  
+
   let hash = 0
-  for (let i = 1; i < username.length; i++) {
+  for (let i = 0; i < username.length; i++) {
     hash += username.charCodeAt(i) * (i + 1) // add position to create variation for similar usernames
   }
 
